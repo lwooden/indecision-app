1454272b6eb9b7d697d3c458691c8ed8b55b4b3a
lwooden <lowell.wooden@gmail.com>
2019-08-15 20:52:53 -0400

    set up base file for JSX Code

---
+++ src/app.js
@@ -0,0 +1,15 @@
+// Will contain our JSX Code
+// This file will be compiled and transformed in /public/scripts/app.js file
+
+console.log("App.js is running!")
+
+// JSX - Javascript XML (Language Extender)
+
+// browser doesn't know what to do with this; we have to compile it down to a language it can understand
+// Babel will do this for us! JSX -> ES5 Javascript
+
+var template = <h1>Indecision App</h1>; 
+
+var appRoot = document.getElementById("app");
+
+ReactDOM.render(template, appRoot)
