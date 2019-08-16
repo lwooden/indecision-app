b5aad64a7b927a7c134d302259e09370e7b03533
lwooden <lowell.wooden@gmail.com>
2019-08-15 20:53:48 -0400

    contains transformed javascript code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,21 @@
-console.log("App.js is running!")
+"use strict";
+
+// Will contain our JSX Code
+// This file will be compiled and transformed in /public/scripts/app.js file
+
+console.log("App.js is running!");
 
 // JSX - Javascript XML (Language Extender)
 
 // browser doesn't know what to do with this; we have to compile it down to a language it can understand
 // Babel will do this for us! JSX -> ES5 Javascript
-// var template = <p>This is JSX from app.js</p>; 
 
-var template = React.createElement("h1", null, "This is JSX from app.js");
+var template = React.createElement(
+  "h1",
+  null,
+  "Indecision App"
+);
 
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot)
+ReactDOM.render(template, appRoot);
