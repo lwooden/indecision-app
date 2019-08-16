83cdc9ea1f01e844957d9a649fc24f85ce52061b
lwooden <lowell.wooden@gmail.com>
2019-08-16 18:37:17 -0400

    automatically updated on save

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 // Will contain our JSX Code
 // This file will be compiled and transformed in /public/scripts/app.js file
@@ -10,10 +10,91 @@ console.log("App.js is running!");
 // browser doesn't know what to do with this; we have to compile it down to a language it can understand
 // Babel will do this for us! JSX -> ES5 Javascript
 
+var app = {
+  title: 'The Indecision App',
+  subTitle: 'Put your life in the hands of a computer!',
+  options: ['One', 'Two']
+};
+
 var template = React.createElement(
-  "h1",
+  'div',
   null,
-  "Indecision App"
+  React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  app.subTitle && React.createElement(
+    'p',
+    null,
+    app.subTitle
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.options.length > 0 ? 'Here are your list of options: ' : 'No options'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item One'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item Two'
+    )
+  )
+);
+
+// Referencing simple variables
+var userName = "Low";
+var userAge = 29;
+var userLocation = 'Maryland';
+
+// Referencing an object
+var user = {
+  name: "Lowell",
+  age: 19,
+  location: 'Maryland'
+
+  // Referencing a function (ES5)
+};function getLocation(location) {
+  if (location) {
+    return React.createElement(
+      'p',
+      null,
+      'Location: ',
+      location
+    );
+  } else {
+    return undefined;
+  }
+}
+
+// Only Javascript "expressions" can go into {}, not conditional logic!
+// We have to define our conditional logic in "functions" and then "call" them
+// If an expression resolves to "undefined" nothing will be rendered in HTML
+// Ternary operator is more precise and it is defined as a statement not an expression so we can call it inline
+
+var template2 = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    user.name ? user.name : 'Anonymous'
+  ),
+  user.age && user.age >= 18 && React.createElement(
+    'p',
+    null,
+    'Age: ',
+    user.age
+  ),
+  getLocation(user.location)
 );
 
 var appRoot = document.getElementById("app");
