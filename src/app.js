9787d6063012bc5150d9b5bdf350ea145b1d742b
lwooden <lowell.wooden@gmail.com>
2019-08-16 18:36:50 -0400

    went over static rendering vs conditional rendering

--- src/app.js
+++ src/app.js
@@ -8,7 +8,58 @@ console.log("App.js is running!")
 // browser doesn't know what to do with this; we have to compile it down to a language it can understand
 // Babel will do this for us! JSX -> ES5 Javascript
 
-var template = <h1>Indecision App</h1>; 
+var app = {
+    title: 'The Indecision App',
+    subTitle: 'Put your life in the hands of a computer!',
+    options: ['One', 'Two']
+}
+
+var template = (
+    <div> 
+      <h1>{app.title}</h1>
+      {app.subTitle && <p>{app.subTitle}</p>}
+      <p>{app.options.length > 0 ? 'Here are your list of options: '  : 'No options'}</p>
+      <ol>
+        <li>Item One</li>
+        <li>Item Two</li>
+      </ol>
+    </div>
+); 
+
+
+// Referencing simple variables
+var userName = "Low";
+var userAge = 29;
+var userLocation = 'Maryland'
+
+// Referencing an object
+var user = {
+    name: "Lowell",
+    age: 19,
+    location: 'Maryland'
+}
+
+// Referencing a function (ES5)
+function getLocation(location) {
+  if (location) {
+    return <p>Location: {location}</p>;
+  } else { 
+    return undefined;
+  }
+}
+
+// Only Javascript "expressions" can go into {}, not conditional logic!
+// We have to define our conditional logic in "functions" and then "call" them
+// If an expression resolves to "undefined" nothing will be rendered in HTML
+// Ternary operator is more precise and it is defined as a statement not an expression so we can call it inline
+
+var template2 = (
+    <div>
+      <h1>{user.name ? user.name : 'Anonymous'}</h1>
+      {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+      {getLocation(user.location)}
+    </div>
+);
 
 var appRoot = document.getElementById("app");
 
